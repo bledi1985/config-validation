06c10a0484320baddd960ea383740170f32f9322
bledi <bledi@faire.ai>
2021-10-30 17:10:41 +0200

    Initial commit

---
+++ src/main/java/com/example/configvalidation/ConfigValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.configvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/configvalidation/ConfigValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
